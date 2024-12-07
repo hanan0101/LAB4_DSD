65091311c83d9121c68de194e2df3f5a3bd027a5
hanan0101 <149705007+hanan0101@users.noreply.github.com>
2024-12-07 19:33:43 +0300

    Update counter_nbit.sv

--- counter_nbit.sv
+++ counter_nbit.sv
@@ -1,63 +1,35 @@
 `timescale 1ns / 1ps
-module counter4bi(
-  input logic clk,
-  input logic reset,
-  input logic en,
-  input logic upDown,
-  input logic load,
-  input logic [3:0] d,
-  output logic [3:0] q
+module counter4b#(
+    parameter n = 8
+)(
+    input  logic clk,
+    input  logic resetn,
+    input  logic load,
+    input  logic clr,
+    input  logic en,
+    input  logic up_down,
+    input  logic [n - 1: 0] d,
+    output logic [n - 1: 0] count
 );
-  logic [3:0] count;
-  logic [3:0] out;
-  logic [3:0] down_count;
-  logic [3:0] up_count;
-  load_mux #(4) p_load(
-    .load(load),
-    .new_data(d),
-    .count(down_count), // Connect count_updown.count to load_mux
-    .out(out)
-  );
-  
-// Apply equations from K-maps for down_count
-assign down_count[0] = ~q[0];
-assign down_count[1] = q[1] ^ q[0];
-assign down_count[2] = q[2] ^ (q[1] & q[0]);
-assign down_count[3] = q[3] ^ (q[2] & q[1] & q[0]);
-// Apply equations from K-maps for up_count
-assign d[0] = ~q[0];
-assign d[1] = (~q[0]) ^ (q[1]);
-assign d[2] = (q[2]) ^ (~q[1] & ~q[0]);
-assign d[3] = (q[3]) ^ (~q[1] & ~q[0] & ~q[2]);  
-  logic en_clk;
-  assign en_clk = clk & en;
-  
-  flipflop ff1(
-    .clk(en_clk),
-    .reset(reset),
-    .d(out[3]),
-    .Q(q[3])
-  );
-  
-  flipflop ff2(
-    .clk(en_clk),
-    .reset(reset),
-    .d(out[2]),
-    .Q(q[2])
-  );
-  
-  flipflop ff3(
-    .clk(en_clk),
-    .reset(reset),
-    .d(out[1]),
-    .Q(q[1])
-  );
-  
-  flipflop ff4(
-    .clk(en_clk),
-    .reset(reset),
-    .d(out[0]),
-    .Q(q[0])
-  );
-  
+    logic [n-1 : 0] counter;
+    
+    always @(posedge clk or negedge resetn) begin 
+        if (~resetn) 
+            counter <= {n{1'b0}}; // Reset count to 0 on active-low reset
+        else if (clr)
+            counter <= 0;
+        else begin 
+            if (load) // Load value d into count if load is high
+                counter <= d;
+            else if (en) begin 
+                if (up_down == 0) // Count up if up_down is low
+                    counter <= counter + 1; 
+                else 
+                    counter <= counter - 1; // Count down if up_down is high
+            end
+        end
+    end
+
+    assign count = counter;
+
 endmodule
